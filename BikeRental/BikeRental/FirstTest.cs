3215e31a0d188fe86ae853fd4f64b8dd48712e77
Rodrigoapa <rodriver999@hotmail.com>
2018-07-07 23:52:44 -0300

    Configuring NUnit Framework

---
+++ BikeRental/BikeRental/FirstTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BikeRental
+{
+    [TestFixture]
+    class FirstTest
+    {
+        [TestCase]
+        public void SimpleTest()
+        {
+            int n = 1;
+            
+            Assert.AreEqual(n, 1);
+        }
+
+
+
+    }
+}
